ff7f7d1891b3e5036bbb7ba347a9be2cd6a62058
Lisa Westlund <lw222gu@student.lnu.se>
2016-03-16 20:40:32 +0100

    Gets locations from API and displays them on the map. A resource service needs to be implemented to handle all requests.

--- client/app/scripts/controllers/simpleMap.js
+++ client/app/scripts/controllers/simpleMap.js
@@ -8,7 +8,35 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-.controller('SimpleMapCtrl', ['$scope', function($scope){
+.controller('SimpleMapCtrl', ['$scope', 'API', '$http', function($scope, API, $http){
+
+  var markers = {};
+
+  var jsonMarkers;
+
+  var url = API.Url + '/locations';
+  var config = {
+    headers: {
+      'X-ApiKey': API.ApiKey,
+      'Accept': 'application/json'
+    }
+  };
+  var promise = $http.get(url, config);
+  promise.success(function(data, status, headers, config){
+    jsonMarkers = data;
+    for(var i = 0; i < jsonMarkers.locations.length; i++){
+      var marker = {
+        lat: parseFloat(jsonMarkers.locations[i]['latitude']),
+        lng: parseFloat(jsonMarkers.locations[i]['longitude']),
+        draggable: false
+      }
+      markers[i] = marker;
+    }
+  });
+  promise.error(function(data, status, headers, config){
+    console.log('FEL');
+  });
+
   angular.extend($scope, {
     center: {
       lat: 59.3,
@@ -20,14 +48,7 @@ angular.module('clientApp')
       maxZoom: 15,
       scrollWheelZoom: false
     },
-    markers: {
-      osloMarker: {
-        lat: 59.91,
-        lng: 10.75,
-        message: "I want to travel here!",
-        focus: true,
-        draggable: false
-      }
-    }
+    markers: markers
   })
+
 }]);
